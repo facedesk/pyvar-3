ef84333017bf1fcdd8175564bbdbd3c741c2fdc5
slosar1 <lslosar@sjnma.org>
2017-09-28 15:03:34 -0500

    Problem4 complete

---
+++ problem3.py
@@ -0,0 +1,11 @@
+person = input("What is your Full name:")
+print("First Name", person)
+print("Middle Name", person)
+print("Last Name", person)
+
+input = ('First Name')
+print ('First Name' , str_('Middle Name"'))
+input = ('Middle Name')
+print = ('Middle Name',str_('Last Name'))
+input = ('Last Name')
+
---
+++ problem4.py
@@ -0,0 +1,8 @@
+story = input("Write a story:")
+first_string = story
+second_string = first_string.replace("the", "ermagherd")
+third_string = second_string.replace("a", "ayyyyyy")
+fourth_string = third_string.replace(".", "\a")
+print(first_string, second_string, third_string, fourth_string)
+
+
